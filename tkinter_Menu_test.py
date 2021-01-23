df626a3aaf6ea277545ed20e135727871b740d50
pmrgraphics <40840643+pmrgraphics@users.noreply.github.com>
2021-01-23 14:18:17 +0000

    tkinter menu test - first graph

--- tkinter_Menu_test.py
+++ tkinter_Menu_test.py
@@ -1,11 +1,22 @@
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+
 import tkinter as tk
+from tkinter import ttk
 
 LARGE_FONT = ('Verdana', 12)
+
 
 class SeaofBTCapp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+
+
+        tk.Tk.wm_title(self, 'Sea of BTC client')
+
         container = tk.Frame(self)
         container.pack(side='top', fill='both', expand='True')
 
@@ -13,7 +24,7 @@ class SeaofBTCapp(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, PageOne, PageTwo, PageThree):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky='nsew')
@@ -23,18 +34,22 @@ class SeaofBTCapp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
-def qf(params):
-    print(params)
 
 class StartPage(tk.Frame):
 
-    def __init__(self, parent, contoller):
+    def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text='Start Page', font=LARGE_FONT)
+        label = tk.Label(self, text='Start Page !!!!!', font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text='Visit page 1', command=lambda: contoller.show_frame(PageOne))
+        button1 = ttk.Button(self, text='Visit Page 1', command=lambda: controller.show_frame(PageOne))
         button1.pack()
+
+        button2 = ttk.Button(self, text='Visit Page 2', command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+        button3 = ttk.Button(self, text='Visit Graph Page', command=lambda: controller.show_frame(PageThree))
+        button3.pack()
 
 class PageOne(tk.Frame):
     def __init__(self, parent, contoller):
@@ -42,10 +57,10 @@ class PageOne(tk.Frame):
         label = tk.Label(self, text='Page One !!!!!', font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text='Back to home', command=lambda: contoller.show_frame(StartPage))
+        button1 = ttk.Button(self, text='Back to home', command=lambda: contoller.show_frame(StartPage))
         button1.pack()
 
-        button2 = tk.Button(self, text='Page Two', command=lambda: contoller.show_frame(PageTwo))
+        button2 = ttk.Button(self, text='Visit Page 2', command=lambda: contoller.show_frame(PageTwo))
         button2.pack()
 
 class PageTwo(tk.Frame):
@@ -54,12 +69,35 @@ class PageTwo(tk.Frame):
         label = tk.Label(self, text='Page Two !!!!!', font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text='Back to home', command=lambda: contoller.show_frame(StartPage))
+        button1 = ttk.Button(self, text='Back to home', command=lambda: contoller.show_frame(StartPage))
         button1.pack()
 
-        button2 = tk.Button(self, text='Page One', command=lambda: contoller.show_frame(PageOne))
+        button2 = ttk.Button(self, text='Visit Page 1', command=lambda: contoller.show_frame(PageOne))
         button2.pack()
+
+class PageThree(tk.Frame):
+    def __init__(self, parent, contoller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text='Graph Page!', font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = ttk.Button(self, text='Back to home', command=lambda: contoller.show_frame(StartPage))
+        button1.pack()
+
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8], [4,8,2,4,7,5,2,8])
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+
+
+
+
 
 
 app = SeaofBTCapp()
+app.geometry('600x400')
 app.mainloop()
